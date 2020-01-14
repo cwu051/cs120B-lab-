dfb199045dfeb21e2b51cd2d3dc953870152f0f1
Chad Wu <cwu051@wch136-30.cs.ucr.edu>
2020-01-14 13:22:04 -0800

    save

--- Lab3_BitManip/source/main.c
+++ Lab3_BitManip/source/main.c
@@ -14,10 +14,29 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+DDRA = 0x00; PORTA = 0xFF;
+DDRB = 0x00; PORTB = 0xFF;
+DDRC = 0xFF; PORTC = 0x00;
+unsigned char tempB = 0x00;
+unsigned char countA = 0x00;
+unsigned char countB = 0x00;
     /* Insert your solution below */
     while (1) {
-
+countA = PINA;
+countB = PINB;
+for (int i = 0; i < 8; i++) {
+if (countA & 0x01) {
+tempB = tempB + 1;
+}
+countA = countA << 1;
+}
+for (int j = 0; j < 8; j++) {
+if (countB & 0x01) {
+tempB = tempB + 1;
+}
+countB = countB << 1;
+}
+PORTC = tempB;
     }
     return 1;
 }
